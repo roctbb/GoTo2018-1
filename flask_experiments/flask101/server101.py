f1c1527e93b8de0e2585b2bf49b8be7df266f7cd
roctbb <roctbb@gmail.com>
2018-06-26 15:46:08 +0300

    more flask 101

--- flask_experiments/flask101/server101.py
+++ flask_experiments/flask101/server101.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,5 +8,31 @@ def hello():
 @app.route("/cat")
 def cats():
     return render_template('cats.html')
+
+@app.route("/hello")
+def greeting():
+    if "name" in request.args:
+        name = request.args['name']
+        return "Hello, {}!".format(name)
+    else:
+        return "<img src='http://pbs.twimg.com/media/CLeVxPeW8AAI-OF.jpg'>" \
+               "<form><input type='text' name='name'/><input type='submit'></form>"
+
+@app.route("/hello_smart")
+def greeting_smart():
+    name = request.args.get('name', 'Анонимус')
+    return "Hello, {}!".format(name)
+
+@app.route("/story")
+def story():
+    first_name = request.args.get('first_name', '')
+    second_name = request.args.get('second_name', '')
+
+    if first_name == '' or second_name == '':
+        return render_template('empty_story.html')
+    else:
+        return render_template('story.html', first=first_name,
+                               second=second_name)
+
 
 app.run()
